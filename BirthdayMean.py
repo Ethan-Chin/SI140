7093414303cc4793cb3b99ca0c64e8255e059181
Eason Brande <easonbrande@gmail.com>
2020-10-30 00:41:05 +0800

    updated

--- BirthdayMean.py
+++ BirthdayMean.py
@@ -1,8 +1,10 @@
 Ex = 2
+Vx = 1
 p = [1]*366
 
 for j in range(2, 366):
     p[j] = p[j-1]*(1-(j - 1)/365)
     Ex += p[j]
-
-print(Ex)
+    Vx += j*p[j]
+Vx = Ex - Ex**2 + 2*Vx
+print("Ex = ", Ex, "Vx = ", Vx)
